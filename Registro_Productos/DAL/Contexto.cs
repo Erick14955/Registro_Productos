fa3d12771f6aab2a2ca88dfc9c4f92e4f4a19ca8
Erickson Fana Morales <erickson_fana@ucne.edu.do>
2021-06-02 17:45:59 -0400

    Add project files.

---
+++ Registro_Productos/DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registro_Productos.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data/DatosProductos");
+        }
+    }
+}
